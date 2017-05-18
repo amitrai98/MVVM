d98cada6ea6fcd9fd66ba519a8f0e590a98275ad
amit rai <bipulamit@gmail.com>
2017-05-18 12:10:47 +0530

    refined library

--- app/src/main/java/com/rai/app/mvvm/presentation/base/BaseViewModel.java
+++ app/src/main/java/com/rai/app/mvvm/presentation/base/BaseViewModel.java
@@ -1,17 +1,16 @@
 package com.rai.app.mvvm.presentation.base;
 
 
-import android.databinding.BaseObservable;
 
-public class BaseViewModel extends BaseObservable implements IViewModel {
-
-    @Override
-    public void onResume() {
-
-    }
-
-    @Override
-    public void onPause() {
-
-    }
-}
+//public class BaseViewModel extends BaseObservable implements IViewModel {
+//
+//    @Override
+//    public void onResume() {
+//
+//    }
+//
+//    @Override
+//    public void onPause() {
+//
+//    }
+//}
--- app/src/main/java/com/rai/app/mvvm/presentation/home/HomeFragment.java
+++ app/src/main/java/com/rai/app/mvvm/presentation/home/HomeFragment.java
@@ -2,19 +2,17 @@ package com.rai.app.mvvm.presentation.home;
 
 
 import android.Manifest;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.app.runtimepermission.PermissionHandler;
 import com.app.runtimepermission.PermissionListener;
 import com.rai.app.mvvm.R;
-import com.rai.app.mvvm.databinding.FragmentHomeBinding;
-import com.rai.app.mvvm.modals.Task;
 import com.rai.app.mvvm.presentation.base.BaseFragment;
 
 /**
@@ -22,49 +20,16 @@ import com.rai.app.mvvm.presentation.base.BaseFragment;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class HomeFragment extends BaseFragment implements View.OnClickListener{
 
     private final String TAG = getClass().getSimpleName();
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private Button btn_isCritical, btn_getSinglePermission, btn_getAllPermission;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -76,16 +41,39 @@ public class HomeFragment extends BaseFragment {
     }
 
     private void init(View view){
+        btn_isCritical = (Button) view.findViewById(R.id.btn_isCritical);
+        btn_getSinglePermission = (Button) view.findViewById(R.id.btn_getSinglePermission);
+        btn_getAllPermission = (Button) view.findViewById(R.id.btn_getAllPermission);
 
-        PermissionHandler handler = new PermissionHandler();
-        handler.getPermission(getActivity(), Manifest.permission.READ_CONTACTS, new PermissionListener() {
+        btn_isCritical.setOnClickListener(this);
+        btn_getSinglePermission.setOnClickListener(this);
+        btn_getAllPermission.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_getSinglePermission:
+                getSinglePermission();
+                break;
+            case R.id.btn_isCritical:
+                checkCriticalPermission();
+                break;
+            case R.id.btn_getAllPermission:
+                getAllPermissions();
+                break;
+
+        }
+    }
+
+
+    /**
+     * gets all the critical permission from the manifest.
+     */
+    private void getAllPermissions(){
+        PermissionHandler.getAllPermissions(getActivity(), new PermissionListener() {
             @Override
             public void onGranted(String message) {
-                Log.e(TAG, message);
-            }
-
-            @Override
-            public void onRejected(String message) {
                 Log.e(TAG, message);
             }
 
@@ -93,11 +81,50 @@ public class HomeFragment extends BaseFragment {
             public void onError(String errorMessage) {
                 Log.e(TAG, errorMessage);
             }
-        });
 
-        Task task = new Task("have to go the the market", "pending");
-        FragmentHomeBinding binding = DataBindingUtil.setContentView(getActivity(), R.layout.fragment_home);
-        binding.setTask(task);
+            @Override
+            public void onRejected(String message) {
+                Log.e(TAG, message);
+            }
+        });
     }
+
+    /**
+     * checks if the permission is critical or not
+     */
+    private void checkCriticalPermission(){
+        boolean value  = PermissionHandler.isCriticalPermission(getContext(),
+                Manifest.permission.INTERNET);
+
+        Log.e(TAG , Manifest.permission.INTERNET+" "+value);
+
+        boolean pValue  = PermissionHandler.isCriticalPermission(getContext(),
+                Manifest.permission.READ_CALENDAR);
+
+        Log.e(TAG , Manifest.permission.READ_CALENDAR+" "+pValue);
+    }
+
+    /**
+     * gets single permission
+     */
+    private void getSinglePermission(){
+        PermissionHandler.getPermission(getActivity(), Manifest.permission.READ_CONTACTS, new PermissionListener() {
+            @Override
+            public void onGranted(String message) {
+                Log.e(TAG, message);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                Log.e(TAG, errorMessage);
+            }
+
+            @Override
+            public void onRejected(String message) {
+                Log.e(TAG, message);
+            }
+        });
+    }
+
 
 }
